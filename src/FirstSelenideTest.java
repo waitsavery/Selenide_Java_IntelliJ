4e4239df7499293b4af6f3e91000b344dada1b6e
waitsavery <wwavery0352@yahoo.com>
2015-09-16 15:26:54 -0400

    pushing from IntelliJ

---
+++ src/FirstSelenideTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+import openqa
+/**
+ * Created by temp on 9/16/2015.
+ */
+
+public class FirstSelenideTest {
+
+    @Test
+    public void test(){
+        System.out.println("Yo");
+
+        WebDriver driver = new FirefoxDriver();
+    }
+}
